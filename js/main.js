22604e9fc8c0f71adb54d175708982bb7306fedd
Alexander Komarov <komar81@gmail.com>
2019-06-17 20:09:16 +0300

    Выполнил module4-task1

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 'use strict';
 var mockDataQuantity = 8;
 var OFFER_TYPE = ['palace', 'flat', 'house', 'bungalo'];
-var MAPS_PINS_BLOCK = document.querySelector('.map__pins');
-var MAP_FADE_BLOCK = document.querySelector('.map');
+var MAP_BLOCK = document.querySelector('.map');
+var MAP_PINS_BLOCK = MAP_BLOCK.querySelector('.map__pins');
+var MAIN_PIN = MAP_PINS_BLOCK.querySelector('.map__pin--main');
 var PIN_ELEMENT_TEMPLATE = document.querySelector('#pin').content.querySelector('.map__pin');
+var PIN_WIDTH = 50; // defined in css
+var PIN_HEIGHT = 70; // defined in css
+var MAP_TOP = 130;
+var MAP_BOTTOM = 630;
+var FORM_FIELDSETS = document.querySelectorAll('form fieldset');
+var MAIN_FORM = document.querySelector('.ad-form');
+var ADDRESS_FIELD = document.querySelector('#address');
 
 var getRandomValue = function (min, max) {
   return Math.floor(Math.random() * (max - min) + min);
@@ -11,7 +19,7 @@ var getRandomValue = function (min, max) {
 
 var generetaMock = function (dataQuantity) {
   var mock = [];
-  var mapPinsBlockWidth = MAPS_PINS_BLOCK.offsetWidth;
+  var mapPinsBlockWidth = MAP_PINS_BLOCK.offsetWidth;
 
   for (var i = 0; i < parseInt(dataQuantity, 10); i++) {
     mock[i] = {
@@ -23,39 +31,124 @@ var generetaMock = function (dataQuantity) {
       },
       'location': {
         'x': getRandomValue(0, mapPinsBlockWidth),
-        'y': getRandomValue(130, 630)
+        'y': getRandomValue(MAP_TOP, MAP_BOTTOM)
       }
     };
   }
   return mock;
 };
 
-var switchMapFade = function () {
-  if (MAP_FADE_BLOCK.classList.contains('map--faded')) {
-    MAP_FADE_BLOCK.classList.remove('map--faded');
-  } else {
-    MAP_FADE_BLOCK.classList.add('map--faded');
+var fadeMap = function () {
+  if (!MAP_BLOCK.classList.contains('map--faded')) {
+    MAP_BLOCK.classList.add('map--faded');
   }
 };
+
+var unFadeMap = function () {
+  if (MAP_BLOCK.classList.contains('map--faded')) {
+    MAP_BLOCK.classList.remove('map--faded');
+  }
+};
+
+var isMapFaded = function () {
+  return MAP_BLOCK.classList.contains('map--faded');
+};
+
+var disableFormFieldsets = function () {
+  for (var i = 0; i < FORM_FIELDSETS.length; i++) {
+    FORM_FIELDSETS[i].disabled = true;
+  }
+};
+
+var enebaleFormFieldsets = function () {
+  for (var i = 0; i < FORM_FIELDSETS.length; i++) {
+    FORM_FIELDSETS[i].disabled = false;
+  }
+};
+
+var enableForm = function () {
+  if (MAIN_FORM.classList.contains('ad-form--disabled')) {
+    MAIN_FORM.classList.remove('ad-form--disabled');
+  }
+};
+
+var disableForm = function () {
+  if (MAIN_FORM.classList.contains('ad-form--disabled')) {
+    MAIN_FORM.classList.add('ad-form--disabled');
+  }
+};
+
+var activatePage = function () {
+  unFadeMap();
+  enableForm();
+  enebaleFormFieldsets();
+};
+
+var disactivatePage = function () {
+  fadeMap();
+  disableForm();
+  disableFormFieldsets();
+}
 
 var createMapPinElement = function (pinData) {
   var pinElement = PIN_ELEMENT_TEMPLATE.cloneNode(true);
   var pinElementImg = pinElement.querySelector('img');
+  var leftCoord = (pinData.location.x + (PIN_WIDTH / 2));
+  if (leftCoord > (MAP_PINS_BLOCK.offsetWidth - (PIN_WIDTH / 2))) {
+    leftCoord = MAP_PINS_BLOCK.offsetWidth - (PIN_WIDTH / 2);
+  } else
+  if (leftCoord < PIN_WIDTH / 2) {
+    leftCoord = PIN_WIDTH / 2;
+  }
+  var topCoord = pinData.location.y - PIN_HEIGHT;
+  if (topCoord < MAP_TOP) {
+    topCoord = MAP_TOP;
+  }
   pinElementImg.src = pinData.author.avatar;
-  pinElement.style.left = pinData.location.x + 'px';
-  pinElement.style.top = pinData.location.y + 'px';
+  pinElement.style.left = leftCoord + 'px';
+  pinElement.style.top = topCoord + 'px';
   pinElementImg.alt = 'Заголовок объявления';
   return pinElement;
 };
 
 var appendPins = function (pinsData) {
   var pinsFragment = document.createDocumentFragment();
-  var pinsBlock = document.querySelector('.map__pins');
   for (var i = 0; i < pinsData.length; i++) {
     pinsFragment.appendChild(createMapPinElement(pinsData[i]));
   }
-  pinsBlock.appendChild(pinsFragment);
+  MAP_PINS_BLOCK.appendChild(pinsFragment);
 };
 
-switchMapFade();
+var onMainPinClick = function () {
+  activatePage();
+  setAddress(getMainPinCoordinates());
+};
+
+var getMainPinSize = function () {
+  var mainPinSize = {};
+  if (isMapFaded()) {
+    mainPinSize.width = MAIN_PIN.offsetWidth;
+    mainPinSize.height = MAIN_PIN.offsetHeight;
+  } else {
+    mainPinSize.width = MAIN_PIN.offsetWidth;
+    mainPinSize.height = MAIN_PIN.offsetHeight + parseInt(window.getComputedStyle(MAIN_PIN, '::after').height, 10);
+  }
+  return mainPinSize;
+};
+
+var getMainPinCoordinates = function () {
+  var coordinates = {};
+  var mainPinSize = getMainPinSize();
+  coordinates.left = MAIN_PIN.offsetLeft + (mainPinSize.width / 2);
+  coordinates.top = MAIN_PIN.offsetTop + (mainPinSize.height / 2);
+  return coordinates;
+};
+
+var setAddress = function (coordinates) {
+  ADDRESS_FIELD.value = coordinates.left + ', ' + coordinates.top;
+}
+
+disactivatePage();
 appendPins(generetaMock(mockDataQuantity));
+setAddress(getMainPinCoordinates());
+MAIN_PIN.addEventListener('click', onMainPinClick);
